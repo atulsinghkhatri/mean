d12cc8759dc484bdbb021c1dc1848257832da2e2
Atul Singh Khatri <atulsinghkhatri@gmail.com>
2020-01-08 16:01:36 +0530

    Add body-parser

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/:name?', (req, res) => { 
+	// For GET Request 
+	console.log(req.query);
+	// For Paramaterized Request 
+	//console.log(req.params.name);
+	// For POST Request 
+	// console.log(req.body);
+	res.send("response");
+})
 
-app.get('/', (req, res) => { res.send('Hello World!') })
-
-app.listen(3000, () => { console.log(`server connected`) })
+app.listen(3000, () => {
+ console.log(`server connected`) 
+})
